00e33f6145b50093e1077a239559f6dfac6c6539
Srdjan Lazarevic <slazarevic1995@gmail.com>
2022-02-20 18:17:43 +0100

    Introduce clang-format
    - Clang-format is supposed to be run via bazel

---
+++ linter/clang-format.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function usage() {
+    echo "Usage: bazel run //linter:clang-format"
+    exit 1
+}
+
+CMD="clang-format"
+
+if [[ -z "$BUILD_WORKSPACE_DIRECTORY" ]]; then
+    echo "Use bazel if you want to run the script"
+    usage
+fi
+
+if ! [[ -x "$(command -v $CMD)" ]]; then
+    echo "$CMD not found, you can install it with sudo apt install clang-format"
+fi
+
+pushd "${BUILD_WORKSPACE_DIRECTORY}" &>/dev/null
+find . \
+    \( -name '*.c' \
+    -o -name '*.cc' \
+    -o -name '*.cpp' \
+    -o -name '*.h' \
+    -o -name '*.hh' \
+    -o -name '*.hpp' \) \
+    -exec "${CMD}" -i '{}' \;
+popd &>/dev/null
