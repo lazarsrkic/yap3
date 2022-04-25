bb9ca27177bcf8477a04f37938c97a2489413faa
Srdjan Lazarevic <slazarevic1995@gmail.com>
2022-04-25 22:28:01 +0200

    Implement skeleton for presentation layer

--- source/layers/presentation.cc
+++ source/layers/presentation.cc
@@ -2,8 +2,15 @@
 #include "layers/presentation.h"
 #include "layers/transport.h"
 
+#include <spdlog/spdlog.h>
+
 namespace yap3 {
 namespace layers {
+
+Presentation::Presentation() noexcept {
+    std::memset(m_buffer.data(), 0x00, presentation::LAYER_MAX_PAYLOAD_SIZE);
+    m_buffer_size = 0;
+}
 
 void Presentation::wrapping_layers(
     std::shared_ptr<Application> application_layer,
@@ -12,14 +19,81 @@ void Presentation::wrapping_layers(
     m_transport_layer = transport_layer;
 }
 
-bool Presentation::encrypt(std::int8_t const* buffer,
-                           std::uint8_t const size) noexcept {
-    return true;
+bool Presentation::encrypt(std::uint8_t const* buffer,
+                           std::size_t const size) noexcept {
+    std::memset(m_buffer.data(), 0x00, presentation::LAYER_MAX_PAYLOAD_SIZE);
+    m_buffer_size = 0;
+
+    if (m_transport_layer) {
+        if (buffer != nullptr &&
+            (size <= presentation::LAYER_MAX_PAYLOAD_SIZE &&
+             size >= presentation::LAYER_IPC_PAYLOAD_OFFSET)) {
+            switch (static_cast<presentation::EncryptType>(
+                buffer[presentation::LAYER_APP_CONTROL_OFFSET])) {
+                case presentation::EncryptType::kTypeA:
+                    break;
+                case presentation::EncryptType::kTypeB:
+                    break;
+                case presentation::EncryptType::kNone:
+                default:
+                    m_buffer[presentation::LAYER_APP_CONTROL_OFFSET] =
+                        buffer[presentation::LAYER_APP_CONTROL_OFFSET];
+                    std::memcpy(
+                        m_buffer.data() +
+                            static_cast<size_t>(
+                                presentation::LAYER_IPC_PAYLOAD_OFFSET),
+                        buffer + static_cast<size_t>(
+                                     presentation::LAYER_IPC_PAYLOAD_OFFSET),
+                        (size - static_cast<size_t>(
+                                    presentation::LAYER_IPC_PAYLOAD_OFFSET)));
+                    m_buffer_size = size;
+                    // call function to build frame for transport layer
+                    return true;
+            }
+        } else {
+            spdlog::error("{} error:Incorrect buffer provided", __func__);
+        }
+    }
+    return false;
 }
 
-bool Presentation::decrypt(std::int8_t const* buffer,
-                           std::uint8_t const size) noexcept {
-    return true;
+bool Presentation::decrypt(std::uint8_t const* buffer,
+                           std::size_t const size) noexcept {
+    std::memset(m_buffer.data(), 0x00, presentation::LAYER_MAX_PAYLOAD_SIZE);
+    m_buffer_size = 0;
+
+    if (m_application_layer) {
+        if (buffer != nullptr &&
+            (size <= presentation::LAYER_MAX_PAYLOAD_SIZE &&
+             size >= presentation::LAYER_IPC_PAYLOAD_OFFSET)) {
+            switch (static_cast<presentation::EncryptType>(
+                buffer[presentation::LAYER_APP_CONTROL_OFFSET])) {
+                case presentation::EncryptType::kTypeA:
+                    break;
+                case presentation::EncryptType::kTypeB:
+                    break;
+                case presentation::EncryptType::kNone:
+                default:
+                    m_buffer[presentation::LAYER_APP_CONTROL_OFFSET] =
+                        buffer[presentation::LAYER_APP_CONTROL_OFFSET];
+                    std::memcpy(
+                        m_buffer.data() +
+                            static_cast<size_t>(
+                                presentation::LAYER_IPC_PAYLOAD_OFFSET),
+                        buffer + static_cast<size_t>(
+                                     presentation::LAYER_IPC_PAYLOAD_OFFSET),
+                        (size - static_cast<size_t>(
+                                    presentation::LAYER_IPC_PAYLOAD_OFFSET)));
+                    m_buffer_size = size;
+                    // call function to send message to application layer
+                    return true;
+            }
+        } else {
+            spdlog::error("{} error:Incorrect buffer provided", __func__);
+        }
+    }
+
+    return false;
 }
 
 }  // namespace layers
--- source/layers/presentation.h
+++ source/layers/presentation.h
@@ -1,32 +1,43 @@
 #ifndef SOURCE_LAYERS_PRESENTATION_H_
 #define SOURCE_LAYERS_PRESENTATION_H_
 
+#include "configuration.h"
+
+#include <array>
 #include <cstdint>
 #include <memory>
+
 namespace yap3 {
 namespace layers {
 
 class Application;
 class Transport;
 namespace presentation {
+constexpr std::uint8_t LAYER_MAX_PAYLOAD_SIZE{
+    yap3::configuration::APP_CONTROL_SIZE +
+    yap3::configuration::MAX_IPC_MSG_PAYLOAD_SIZE};
+constexpr std::uint8_t LAYER_IPC_PAYLOAD_OFFSET{
+    yap3::configuration::APP_CONTROL_SIZE};
+constexpr std::uint8_t LAYER_APP_CONTROL_OFFSET{0U};
+
 enum class EncryptType : std::uint8_t { kNone, kTypeA, kTypeB };
 }  // namespace presentation
 
 class Presentation {
 public:
-    Presentation(presentation::EncryptType const& type =
-                     presentation::EncryptType::kNone){};
+    Presentation() noexcept;
 
     void wrapping_layers(std::shared_ptr<Application> application_layer,
                          std::shared_ptr<Transport> transport_layer) noexcept;
 
-    bool encrypt(std::int8_t const* buffer, std::uint8_t const size) noexcept;
-    bool decrypt(std::int8_t const* buffer, std::uint8_t const size) noexcept;
+    bool encrypt(std::uint8_t const* buffer, std::size_t const size) noexcept;
+    bool decrypt(std::uint8_t const* buffer, std::size_t const size) noexcept;
 
 private:
-    presentation::EncryptType encypt_type_;
     std::shared_ptr<Application> m_application_layer;
     std::shared_ptr<Transport> m_transport_layer;
+    std::array<std::uint8_t, presentation::LAYER_MAX_PAYLOAD_SIZE> m_buffer;
+    std::size_t m_buffer_size;
 };
 
 }  // namespace layers
