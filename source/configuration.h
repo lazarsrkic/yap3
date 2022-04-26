c4f51fa470ecff606629b1bf0efa39ec1c821648
Srdjan Lazarevic <slazarevic1995@gmail.com>
2022-04-26 21:15:55 +0200

    Define additional shared parameters

--- source/configuration.h
+++ source/configuration.h
@@ -1,6 +1,8 @@
 #ifndef SOURCE_CONFIGURATION_H_
 #define SOURCE_CONFIGURATION_H_
 
+#include "utils/ringbuffer.h"
+
 #include <cstdint>
 
 namespace yap3 {
@@ -12,6 +14,11 @@ constexpr std::uint8_t FRAME_CONTROL_SIZE{1U};
 constexpr std::uint8_t APP_CONTROL_SIZE{1U};
 
 constexpr std::uint8_t MAX_IPC_MSG_PAYLOAD_SIZE{100U};
+
+constexpr std::uint8_t MAX_RINGBUFFER_SIZE{10U};
+
+typedef yap3::utils::RingBuffer<MAX_RINGBUFFER_SIZE, MAX_IPC_MSG_PAYLOAD_SIZE>
+    rx_ringbuffer_type;
 
 }  // namespace configuration
 }  // namespace yap3
