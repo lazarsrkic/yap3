e18ee55c8a371cb7a8d9b10fdfb03c7a8b3a3fcf
Srdjan Lazarevic <slazarevic1995@gmail.com>
2022-04-27 22:51:06 +0200

    Skeleton implementation for adding message to TX queue

--- source/layers/transport.cc
+++ source/layers/transport.cc
@@ -1,18 +1,61 @@
-#include "layers/application.h"
 #include "layers/datalink.h"
 #include "layers/presentation.h"
 #include "layers/transport.h"
+#include "utils/crc16.h"
 
 namespace yap3 {
 namespace layers {
 
 void Transport::wrapping_layers(
-    std::shared_ptr<Application> application_layer,
     std::shared_ptr<Presentation> presentation_layer,
     std::shared_ptr<Datalink> datalink_layer) noexcept {
-    m_application_layer = application_layer;
     m_presentation_layer = presentation_layer;
     m_datalink_layer = datalink_layer;
+}
+
+bool Transport::process_message(std::uint8_t const* buffer,
+                                std::size_t const size) noexcept {
+    if (buffer == nullptr || size == 0) {
+        return false;
+    }
+
+    std::memset(m_buffer.data(), 0x00, transport::MAX_COMPLETE_MESSAGE_SIZE);
+    m_buffer_size = yap3::configuration::FLAG_SIZE * 2 +
+                    yap3::configuration::CRC_SIZE +
+                    yap3::configuration::FRAME_CONTROL_SIZE + size;
+
+    m_buffer[transport::LAYER_START_FLAG_OFFSET] = transport::START;
+    m_buffer[m_buffer_size - 1] = transport::STOP;
+
+    std::uint16_t crc = yap3::utils::Crc16{}(buffer, size);
+    if (crc == 0) {
+        spdlog::error("{} error: crc calculation failed", __func__);
+        return false;
+    }
+
+    m_buffer[transport::LAYER_CRC_OFFSET] = crc >> 8;
+    m_buffer[transport::LAYER_CRC_OFFSET + 1] = crc & 0xFF;
+
+    m_buffer[transport::LAYER_FRAME_CONTROL_OFFSET] =
+        0;  // TODO: Populate this one
+    std::memcpy(m_buffer.data() + transport::LAYER_IPC_PAYLOAD_OFFSET, buffer,
+                size);
+
+    std::lock_guard<std::mutex> lock{m_tx_ringbuffer_mutex};
+
+    if (!m_tx_ringbuffer.full()) {
+        if (m_tx_ringbuffer.put(m_buffer.data(), m_buffer_size)) {
+            spdlog::info("New message added to tx queue");
+            return true;
+        } else {
+            spdlog::error("Failed to add new message to tx queue!");
+        }
+
+    } else {
+        spdlog::error("Tx queue is full! Failed to add new message!");
+    }
+
+    return false;
 }
 
 }  // namespace layers
--- source/layers/transport.h
+++ source/layers/transport.h
@@ -1,25 +1,62 @@
 #ifndef SOURCE_LAYERS_TRANSPORT_H_
 #define SOURCE_LAYERS_TRANSPORT_H_
 
+#include "configuration.h"
+#include "utils/ringbuffer.h"
+#include "utils/timer.h"
+
+#include <cstdint>
 #include <memory>
+#include <mutex>
 
 namespace yap3 {
 namespace layers {
 
-class Application;
 class Presentation;
 class Datalink;
 
+namespace transport {
+constexpr std::uint8_t MAX_COMPLETE_MESSAGE_SIZE{
+    2 * (yap3::configuration::FLAG_SIZE * 2 + yap3::configuration::CRC_SIZE +
+         yap3::configuration::FRAME_CONTROL_SIZE +
+         yap3::configuration::APP_CONTROL_SIZE +
+         yap3::configuration::MAX_IPC_MESSAGE_PAYLOAD_SIZE)};
+constexpr std::uint16_t READ_BUFFER_SIZE{MAX_COMPLETE_MESSAGE_SIZE * 5};
+
+constexpr std::uint8_t START{0x12U};
+constexpr std::uint8_t STOP{0x13U};
+constexpr std::uint8_t ESCAPE{0x7DU};
+
+constexpr std::uint8_t LAYER_START_FLAG_OFFSET{0U};
+constexpr std::uint8_t LAYER_CRC_OFFSET{yap3::configuration::FLAG_SIZE};
+constexpr std::uint8_t LAYER_FRAME_CONTROL_OFFSET{
+    yap3::configuration::FLAG_SIZE + yap3::configuration::CRC_SIZE};
+constexpr std::uint8_t LAYER_IPC_PAYLOAD_OFFSET{
+    yap3::configuration::FLAG_SIZE + yap3::configuration::CRC_SIZE +
+    yap3::configuration::FRAME_CONTROL_SIZE};
+}  // namespace transport
+
 class Transport {
 public:
-    void wrapping_layers(std::shared_ptr<Application> application_layer,
-                         std::shared_ptr<Presentation> presentation_layer,
+    void wrapping_layers(std::shared_ptr<Presentation> presentation_layer,
                          std::shared_ptr<Datalink> datalink_layer) noexcept;
 
+    bool process_message(std::uint8_t const* buffer,
+                         std::size_t const size) noexcept;
+
 private:
-    std::shared_ptr<Application> m_application_layer;
+    typedef yap3::utils::RingBuffer<yap3::configuration::MAX_RINGBUFFER_SIZE,
+                                    transport::MAX_COMPLETE_MESSAGE_SIZE>
+        tx_ringbuffer_type;
+
     std::shared_ptr<Presentation> m_presentation_layer;
     std::shared_ptr<Datalink> m_datalink_layer;
+
+    std::array<std::uint8_t, transport::MAX_COMPLETE_MESSAGE_SIZE> m_buffer;
+    std::size_t m_buffer_size;
+
+    std::mutex m_tx_ringbuffer_mutex;
+    tx_ringbuffer_type m_tx_ringbuffer{};
 };
 
 }  // namespace layers
